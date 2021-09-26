5965bd6dfdae0978a273851d353f82ec001dba71
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2021-09-27 01:51:23 +0900

    Update: extractor

--- scripts/download.sh
+++ scripts/download.sh
@@ -9,4 +9,4 @@ wget https://datadryad.org/stash/downloads/file_stream/392626 -O ./datasets/bitc
 wget https://datadryad.org/stash/downloads/file_stream/392627 -O ./datasets/bitcoin_2018_txin.dat.xz
 wget https://datadryad.org/stash/downloads/file_stream/392628 -O ./datasets/bitcoin_2018_txout.dat.xz
 wget https://datadryad.org/stash/downloads/file_stream/392642 -O ./datasets/bitcoin_2018_README.txt
-wget https://datadryad.org/stash/downloads/file_stream/392643 -O ./datasets/bitcoin_2018_addr_sccs.dat.gz
+wget https://datadryad.org/stash/downloads/file_stream/392643 -O ./datasets/bitcoin_2018_addr_sccs.dat.xz
--- scripts/extract.sh
+++ scripts/extract.sh
@@ -8,4 +8,4 @@ xz --decompress ./datasets/bitcoin_2018_tx.dat.xz
 gunzip ./datasets/bitcoin_2018_txh.dat.gz
 xz --decompress ./datasets/bitcoin_2018_txin.dat.xz
 xz --decompress ./datasets/bitcoin_2018_txout.dat.xz
-gunzip ./datasets/bitcoin_2018_addr_sccs.dat.gz
+xz --decompress ./datasets/bitcoin_2018_addr_sccs.dat.xz
